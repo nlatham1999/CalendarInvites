f34e4df875795a120e99d2bc6217744267c4f4f3
Nick Latham <nlatham@zagmail.gonzaga.edu>
2021-12-08 21:46:17 -0800

    logic

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -19,8 +19,8 @@ export class MainPageComponent implements OnInit {
   typeSelection = "Wedding";
   intervalSelection = "Weekly";
   eventsNumber = 0;
-  anniversaryDate = "12-31-9999";
-  dateSelection = "12-31-9999";
+  anniversaryDate = "9999-12-31";
+  dateSelection = "9999-12-31";
   inError = false;
   submitted = false;
   useTodaysDate = false;
@@ -37,11 +37,28 @@ export class MainPageComponent implements OnInit {
       return;
     }
     let currentDate = new Date();
-    this.dateSelection = currentDate.getMonth() + "-" + currentDate.getDate() + "-" + currentDate.getFullYear();
+    this.dateSelection = currentDate.getFullYear() + "-" + currentDate.getMonth() + "-" + currentDate.getDate();
   }
 
   setDate(e : any){
     this.dateSelection = e.target.value;
+  }
+
+  addMonthToDate(date : string): string {
+    var x = date.split("-");
+    var month: number = +x[1] + 1;
+    if(month === 13){
+      month = 1;
+    }
+    var newDate: string = x[0] + "-" + month + "-" + x[2];
+    return newDate;
+  }
+
+  addYearToDate(date: string): string {
+    var x = date.split("-");
+    var year: number = +x[2] + 1;
+    var newDate: string = year + "-" + x[1] + "-" + x[2];
+    return newDate;
   }
 
   createFile(): void {
@@ -52,22 +69,70 @@ export class MainPageComponent implements OnInit {
     content += "CALSCALE:GREGORIAN\n";
     content += "METHOD:PUBLISH\n";
 
-    content += this.createEvent("event 1");
-    content += this.createEvent("event 2");
+    content += this.createEvents();
 
     content += "END:VCALENDAR\n";
 
     this.download("event.ics", content);
   }
 
-  createEvent(summary: string): string {
+  createEvents(): string {
+    var content: string = "";
+    var count: number = 0;
+    var anniversaryCount: number = 1;
+    var date: string = this.anniversaryDate;
+    var interval: string = "";
+
+    if(this.intervalSelection === "Weekly"){
+      interval = "Week";
+    }else if(this.intervalSelection === "Monthly"){
+      interval = "Month";
+    }else if(this.intervalSelection === "Yearly"){
+      interval = "Year";
+    }else{
+      return "";
+    }
+
+    console.log(interval);
+
+    while(count < this.eventsNumber){
+      //increment the date
+      if(this.intervalSelection === "Weekly"){
+        date = this.addYearToDate(date);
+      }else if(this.intervalSelection === "Monthly"){
+        date = this.addMonthToDate(date);
+      }else if(this.intervalSelection === "Yearly"){
+        date = this.addYearToDate(date);
+      }
+
+      console.log(date);
+
+      //if the date is older than the start date, then add
+      if(date >= this.dateSelection){
+        console.log(date + " " + this.dateSelection);
+        var summary: string = anniversaryCount + " " + interval + " Anniversary!";
+        var dateSplit = date.split("-");
+        var dtstart: string = dateSplit[0] + dateSplit[1] + dateSplit[2];
+        var dtend: string = dtstart;
+        
+        content += this.createEvent(summary, dtstart, dtend);
+
+        count++;
+      }
+      anniversaryCount++;
+    }
+
+    return content;
+  }
+
+  createEvent(summary: string, startDate: string, endDate: string): string {
     var content = "";
     content += "BEGIN:VEVENT\n";
     content += "SUMMARY:"+summary+"\n";
-    content += "DTSTART:20220212\n";
-    content += "DTEND:20220213\n";
-    content += "CONTENT:hello world\n";
-    content += "DESCRIPTION:Testing Testing\n";
+    content += "DTSTART:"+startDate+"\n";
+    content += "DTEND:"+endDate+"\n";
+    content += "CONTENT:"+summary+"\n";
+    content += "DESCRIPTION:"+summary+"\n";
     content += "END:VEVENT\n";
     return content;
   }
